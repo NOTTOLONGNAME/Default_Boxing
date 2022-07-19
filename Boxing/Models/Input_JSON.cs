fe5df177e3494b5f4e38a643a40cb7136b70dae9
NOTTOLONGNAME <109416665+NOTTOLONGNAME@users.noreply.github.com>
2022-07-19 13:24:18 +0300

    Add files via upload

---
+++ Boxing/Models/Input_JSON.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxing.Models
+{
+    class Input_JSON
+    {
+        [Newtonsoft.Json.JsonProperty("cargo_space")]
+        public Cargo_Space Cargo_space { get; set; }
+        [Newtonsoft.Json.JsonProperty("cargo_groups")]
+        public Cargo_Groups[] Cargo_groups { get; set; }
+    }
+
+    public class Cargo_Space
+    {
+        [Newtonsoft.Json.JsonProperty("id")]
+        public int Id { get; set; }
+        [Newtonsoft.Json.JsonProperty("mass")]
+        public int Mass { get; set; }
+        [Newtonsoft.Json.JsonProperty("size")]
+        public int[] Size { get; set; }
+        public void Write_ALL()
+        {
+            Console.WriteLine($"ID:{Id}\n MASS: {Mass}\n Size: {Size[0]}, {Size[1]}, {Size[2]}\n\n");
+        }
+    }
+
+    public class Cargo_Groups
+    {
+        [Newtonsoft.Json.JsonProperty("id")]
+        public string Id { get; set; }
+        [Newtonsoft.Json.JsonProperty("mass")]
+        public int Mass { get; set; }
+        [Newtonsoft.Json.JsonProperty("size")]
+        public int[] Size { get; set; }
+        [Newtonsoft.Json.JsonProperty("sort")]
+        public int Sort { get; set; }
+        [Newtonsoft.Json.JsonProperty("count")]
+        public int Count { get; set; }
+        [Newtonsoft.Json.JsonProperty("group_id")]
+        public string Group_id { get; set; }
+
+        public void Write_ALL()
+        {
+            Console.WriteLine($"ID:{Id}\n MASS: {Mass}\n Size: {Size[0]}, {Size[1]}, {Size[2]}\n" +
+                $"Sort: {Sort}\n Count: {Count}\n Group_id: {Group_id}\n\n");
+        }
+    }
+
+}
---
+++ Boxing/Models/Output_JSON.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxing.Models
+{
+
+    public class Output_JSON
+    {
+        public Cargospace cargoSpace { get; set; }
+        public Cargo[] cargos { get; set; }
+        public Unpacked[] unpacked { get; set; }
+    }
+
+    public class Cargospace
+    {
+        public Loading_Size loading_size { get; set; }
+        public float[] position { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Loading_Size
+    {
+        public float height { get; set; }
+        public float length { get; set; }
+        public float width { get; set; }
+    }
+
+    public class Cargo
+    {
+        public Calculated_Size calculated_size { get; set; }
+        public string cargo_id { get; set; }
+        public int id { get; set; }
+        public float mass { get; set; }
+        public Position position { get; set; }
+        public Size size { get; set; }
+        public int sort { get; set; }
+        public bool stacking { get; set; }
+        public bool turnover { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Calculated_Size
+    {
+        public float height { get; set; }
+        public float length { get; set; }
+        public float width { get; set; }
+    }
+
+    public class Position
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+    }
+
+    public class Size
+    {
+        public float height { get; set; }
+        public float length { get; set; }
+        public float width { get; set; }
+    }
+
+    public class Unpacked
+    {
+        public string group_id { get; set; }
+        public int id { get; set; }
+        public float mass { get; set; }
+        public Position position { get; set; }
+        public Size size { get; set; }
+        public int sort { get; set; }
+        public bool stacking { get; set; }
+        public bool turnover { get; set; }
+    }
+
+}
