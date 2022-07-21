66bbf14723698b9eccd69ca85468101b92dd4b1b
NOTTOLONGNAME <109416665+NOTTOLONGNAME@users.noreply.github.com>
2022-07-21 09:43:57 +0300

    Add files via upload

--- Boxing/Program.cs
+++ Boxing/Program.cs
@@ -199,9 +199,9 @@ namespace Boxing
                 Hangar.Hangar.fillbox(Package, fin);
                 
                 string PathOut = Directory.GetCurrentDirectory();
-                PathOut = PathOut.Remove(PathOut.Length - 10, 10);
+                PathOut = PathOut.Remove(PathOut.Length - 16, 16);
                 
-                PathOut = Path.Combine(PathOut, "Default\\Output");
+                PathOut = Path.Combine(PathOut, "group1\\Output");
                 string fileName = "Output" + order + ".json";
                 PathOut = Path.Combine(PathOut, fileName);
                 using (FileStream fs = File.Create(PathOut)) 
